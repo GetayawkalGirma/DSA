367fbc4af869c0815ce81d2a6e0631a15ce3e526
Getayawkal Girma <101594145+GetayawkalGirma@users.noreply.github.com>
2023-10-25 23:44:13 +0300

    Create range function.py

---
+++ range function.py
@@ -0,0 +1,14 @@
+# Range from 0 to 4 (exclusive of 5)
+for x in range(5):
+    print(x)
+# Output: 0, 1, 2, 3, 4
+
+# Range from 2 to 6 (exclusive of 7)
+for x in range(2, 7):
+    print(x)
+# Output: 2, 3, 4, 5, 6
+
+# Range from 1 to 10 with a step of 2
+for x in range(1, 11, 2):
+    print(x)
+# Output: 1, 3, 5, 7, 9
